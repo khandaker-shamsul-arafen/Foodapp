003adc1994930f12205b1b55c0706ef3b1d3e4d4
khandaker-shamsul-arafen <khandakershamsul@gmail.com>
2022-09-10 17:36:38 +0600

    Connect apps with Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:food/scr/screens/home.dart';
 import 'package:food/scr/screens/login.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+
+  );
   runApp(const MyApp());
 }
 
